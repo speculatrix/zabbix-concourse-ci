b45bfaf546bc2ccac49813e1e9a9569149b86a78
Paul M <paul-github@mansfield.co.uk>
2019-05-17 09:26:17 +0000

    fixed fly login, easy to make run on other distros

--- concourse_ci.sh
+++ concourse_ci.sh
@@ -2,7 +2,8 @@
 
 ARG1="$1"
 FLY="/usr/local/bin/fly"
-FLYRC="$HOME/.flyrc"
+FLYRC="/home/centos/.flyrc"
+FLYUSER="centos"
 
 # check if we need to log in again - the .flyrc token lasts a day, 86400 seconds
 # so we'll renew just before that.
@@ -10,7 +11,7 @@ NOW_DATE=$( date +%s )
 FLYRC_DATE=$( stat --printf=%Y "$FLYRC" )
 FLY_AGE=$(( $NOW_DATE - $FLYRC_DATE ))
 if [ $FLY_AGE -gt 86000 ] ; then
-	fly -t myteam login -c https://concourse.aws.agileanalog.com -k -u admin -p abc123
+	sudo -u $FLYUSER /usr/local/bin/fly -t aa login -c https://concourse.aws.agileanalog.com -k -u admin -p secret123
 fi
 
 
@@ -22,28 +23,28 @@ fi
 if [ -n "$ARG1" ] ; then
 
 	if [ "$ARG1" == "containers.idle" ] ; then
-		sudo -u centos /usr/local/bin/fly -t myteam containers | awk '{print $3}' | grep none | wc -l
+		sudo -u $FLYUSER /usr/local/bin/fly -t aa containers | awk '{print $3}' | grep none | wc -l
 
 	elif [ "$ARG1" == "containers.total" ] ; then
-		sudo -u centos /usr/local/bin/fly -t myteam containers | wc -l
+		sudo -u $FLYUSER /usr/local/bin/fly -t aa containers | wc -l
 
 	elif [ "$ARG1" == "pipelines.paused" ] ; then
-		sudo -u centos fly -t myteam pipelines | awk '{print $2}' | grep yes | wc -l
+		sudo -u $FLYUSER fly -t aa pipelines | awk '{print $2}' | grep yes | wc -l
 
 	elif [ "$ARG1" == "pipelines.total" ] ; then
-		sudo -u centos fly -t myteam pipelines | wc -l
+		sudo -u $FLYUSER fly -t aa pipelines | wc -l
 
 	elif [ "$ARG1" == "volumes.total" ] ; then
-		sudo -u centos fly -t myteam volumes | wc -l
+		sudo -u $FLYUSER fly -t aa volumes | wc -l
 
 	elif [ "$ARG1" == "workers.running" ] ; then
-		sudo -u centos fly -t myteam workers | grep " running " | wc -l
+		sudo -u $FLYUSER fly -t aa workers | grep " running " | wc -l
 
 	elif [ "$ARG1" == "workers.landed" ] ; then
-		sudo -u centos fly -t myteam workers | grep " landed " | wc -l
+		sudo -u $FLYUSER fly -t aa workers | grep " landed " | wc -l
 
 	elif [ "$ARG1" == "workers.total" ] ; then
-		sudo -u centos fly -t myteam workers | wc -l
+		sudo -u $FLYUSER fly -t aa workers | wc -l
 
 	else
 		echo "Error, parameter not recognised"
